9c58ad7a418c7906be724c5f5d2977703a223501
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2017-06-09 12:50:49 -0400

    04 Terrain

---
+++ WastelandWars/Source/WastelandWars/WastelandWarsGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WastelandWarsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WASTELANDWARS_API AWastelandWarsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
